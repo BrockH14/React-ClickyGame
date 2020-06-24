32a6ec63e2e0ee01789be1c2671b844b7b15bed9
Brock Horton <brock.horton@gmail.com>
2020-06-24 18:49:26 -0400

    heroku hot fix 05

---
+++ build/precache-manifest.1237a582807d4281022b70d1cb3c862f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5fd8292d41dfa50172d60df70d7b3b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d7913762d26204f4f04",
+    "url": "/static/css/main.6e1ca0bc.chunk.css"
+  },
+  {
+    "revision": "4442fb8f9f20f2a6a604",
+    "url": "/static/js/2.c34bda22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c34bda22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d7913762d26204f4f04",
+    "url": "/static/js/main.895dcfda.chunk.js"
+  },
+  {
+    "revision": "76c0879cc130edce90a2",
+    "url": "/static/js/runtime-main.2cea7679.js"
+  }
+]);
